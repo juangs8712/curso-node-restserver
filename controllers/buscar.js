2e256acf2cf47ba5a1e19105e6a320aa6f925508
juangs8712 <juangs8712@gmail.com>
2022-10-01 16:13:58 -0400

    Finalizadas las rutas de busquedas

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -14,22 +14,83 @@ const coleccionesPermitidas = [
 const idNoValido = 'Este ID no corresponde a la coleccion especificada';
 
 // -----------------------------------------------------
+const buscarCategoria = async ( termino = '', res = response ) => {
+    const esMongoID = isValidObjectId( termino );
+
+    if ( esMongoID ) {
+        const coleccion = await Categoria.findById( termino );
+        if ( coleccion ) {
+            return res.json( { 
+                results: [ coleccion ]
+            });            
+        }
+    }
+
+    // hacer la busqueda insensible a mayusculas y minusculas
+    const regex = RegExp( termino, 'i' );    
+    
+    // con .count se cuentan los registros devueltos por la consulta
+    // const users = await Usuario.count( {  
+    const categorias = await Categoria.find( { nombre: regex , estado: true } );
+
+    return res.json({
+        results: categorias? categorias : []
+    })  
+}
+
+// -----------------------------------------------------
 const buscarUsuario = async ( termino = '', res = response ) => {
     const esMongoID = isValidObjectId( termino );
 
     if ( esMongoID ) {
-        const usuario = await Usuario.findById( termino );
-
-        return res.json( { 
-            results: usuario ? [ usuario ] : [] 
-        });
+        const coleccion = await Usuario.findById( termino );
+        if ( coleccion ) {
+            return res.json( { 
+                results: [ coleccion ]
+            });            
+        }
     }
 
-    const usuarios = Usuario.find( { nombre: termino } );
+    // hacer la busqueda insensible a mayusculas y minusculas
+    const regex = RegExp( termino, 'i' );    
 
-    res.json({
-        results: usuarios
-    })
+    const users = await Usuario.find( { 
+        $or:  [ { nombre: regex }, { correo: regex } ],
+        $and: [ { estado: true } ]
+    } );
+
+    return res.json({
+        results: users? users : []
+    })  
+}
+
+// -----------------------------------------------------
+const buscarProducto = async ( termino = '', res = response ) => {
+    const esMongoID = isValidObjectId( termino );
+
+    if ( esMongoID ) {
+        const coleccion = await Producto.findById( termino )
+            .populate( 'categoria', 'nombre');
+        if ( coleccion ) {
+            return res.json( { 
+                results: [ coleccion ]
+            });            
+        }
+    }
+
+    // hacer la busqueda insensible a mayusculas y minusculas
+    const regex = RegExp( termino, 'i' );    
+    
+    // con .count se cuentan los registros devueltos por la consulta
+    // const users = await Usuario.count( {  
+    const productos = await Producto.find( { 
+        $or:  [ { nombre: regex }, { descripcion: regex } ],
+        $and: [ { estado: true } ]
+    } ).populate( 'categoria', 'nombre');;
+
+    return res.json({
+        results: productos? productos : []
+    })  
 }
 
 // -----------------------------------------------------
@@ -44,13 +105,13 @@ export const buscar = async ( req = request, res = response ) => {
 
     switch ( coleccion ) {
         case 'categorias':
-        
+            await buscarCategoria( termino, res );        
         break;
         case 'usuarios':
             await buscarUsuario( termino, res );
         break;
         case 'productos':
-        
+            await buscarProducto( termino, res );                
         break;
         default:
             return res.status( 500 ).json({
